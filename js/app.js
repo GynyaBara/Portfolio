2499ce9b8cd7ce98f9f05761e28089ed18f4c495
GynyaBara <102617101+GynyaBara@users.noreply.github.com>
2023-01-05 14:29:09 +0200

    Added animation

--- js/app.js
+++ js/app.js
@@ -29,3 +29,44 @@ $( document ).ready(function(){
         )
     })
 });
+
+const animItems = document.querySelectorAll("._anim-item");
+if(animItems.length > 0){
+    window.addEventListener('scroll', animOnScroll);
+    function animOnScroll() {    
+
+        for(let index = 0; index < animItems.length; index++){
+           
+            const animItem = animItems[index];
+            const animItemHeight = animItem.offsetHeight;
+            const animItemOffset = offset(animItem).top;
+            const animStart = 2;
+
+            let animItemPoint = window.innerHeight - animItemHeight / animStart;
+            if(animItemHeight > window.innerHeight){
+                animItemPoint = window.innerHeight - window.innerHeight / animStart;
+                
+            }
+            if((scrollY > animItemOffset - animItemPoint) && scrollY < (animItemOffset + animItemHeight)){
+                animItem.classList.add("_active")
+                
+            }else{
+                if(!animItem.classList.contains('_anim-no-hiden')){
+                    animItem.classList.remove("_active")
+                }
+                
+            }
+        }
+    }
+
+    function offset(el){
+        const rect = el.getBoundingClientRect(),
+            scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+            return {top: rect.top + scrollTop}
+    }
+    setTimeout(() =>{
+        animOnScroll();
+    }, 300)
+   
+}
+
